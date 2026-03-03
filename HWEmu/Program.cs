c5dfeb2a6828eb5a53ab59af49707e569e42ec20
JJPaa <juhojarvenpaa98@gmail.com>
2026-03-03 09:37:39 +0200

    Cancel logic when adding new items

--- HWEmu/Program.cs
+++ HWEmu/Program.cs
@@ -6,72 +6,96 @@ namespace HWEmu
 {
     internal class Program
     {
-        public static List<Psu> psus = new List<Psu>();
-        public static List<Inverter> inverters = new List<Inverter>();
+        public static List<Psu> psus = new();
+        public static List<Inverter> inverters = new();
 
-        static void Main(string[] args)
+        public static CancellationTokenSource? _logicCts;
+        public static Task? _logicTask;
+
+        static async Task Main(string[] args)
         {
-            Vector2 psuSize = new Vector2(100, 100);
+            Vector2 psuSize = new(100, 100);
 
             Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
             Raylib.InitWindow(1000, 1000, "HWEmu");
             Raylib.SetTargetFPS(60);
 
+            StartLogic();
+
             while (!Raylib.WindowShouldClose())
             {
-
-                if(Raylib.IsMouseButtonPressed(MouseButton.Left))
+                if (Raylib.IsMouseButtonPressed(MouseButton.Left))
                 {
+                    StopLogic();
+
                     var mousePosVec2 = Raylib.GetMousePosition();
-                    Psu p = new Psu(new Rectangle(mousePosVec2, psuSize));
+                    Psu p = new(new Rectangle(mousePosVec2, psuSize));
                     psus.Add(p);
-                    CalculateStates();
+
+                    StartLogic();
                 }
 
                 if (Raylib.IsMouseButtonPressed(MouseButton.Right))
                 {
-                    Inverter i = new Inverter(Raylib.GetMousePosition());
+                    StopLogic();
+                    Inverter i = new(Raylib.GetMousePosition());
                     inverters.Add(i);
-                    CalculateStates();
+                    StartLogic();
                 }
 
                 Raylib.BeginDrawing();
 
                 foreach (var psu in psus)
-                {
                     Psu.DrawPSU(psu);
-                }
 
                 foreach (var i in inverters)
-                {
                     Inverter.DrawInverter(i);
-                }
 
                 Raylib.EndDrawing();
             }
+
+            StopLogic();
+        }
+
+        private static void StartLogic()
+        {
+            _logicCts = new CancellationTokenSource();
+            _logicTask = Task.Run(() => LogicLoop(_logicCts.Token));
+            Console.WriteLine("Logic started");
+        }
+
+        private static void StopLogic()
+        {
+            var cts = _logicCts;
+            var task = _logicTask;
+
+            if (cts == null || task == null)
+                return;
+
+            _logicCts = null;
+            _logicTask = null;
+
+            cts.Cancel();
+            Console.WriteLine("Logic cancel signal sent");
+            task.GetAwaiter().GetResult();
+            cts.Dispose();
+        }
+
+        private static async Task LogicLoop(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                Console.WriteLine("Logic happening");
+                CalculateStates();
+                await Task.Delay(100);
+            }
+
+            Console.WriteLine("Logic stopped");
         }
 
         private static void CalculateStates()
         {
-            foreach (var psu in psus)
-            {
-                // Determine where is the shortest path and calculate the state change there
-                // This needs a little bit more thought...
-                // Making inverter input and output to meet should not cause any odd cases
 
-                //foreach(var i in inverters)
-                //{
-                //    if(Raylib.CheckCollisionRecs(psu.Bounds, i.InLine))
-                //    {
-                //        i.InState = true;
-                //        // set outState false if not forced to stay true
-                //        if(i.OutState != Enums.State.ForcedOn)
-                //        {
-                //            i.OutState = Enums.State.Off;
-                //        }
-                //    }
-                //}
-            }
         }
     }
 }
