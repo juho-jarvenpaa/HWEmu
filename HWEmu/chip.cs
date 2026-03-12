7a6c58cf20a6b93cfd1e79437a2b82de7b1c9c80
JJPaa <juhojarvenpaa98@gmail.com>
2026-03-12 09:44:24 +0200

    temp

---
+++ HWEmu/chip.cs
@@ -0,0 +1,36 @@
+﻿using Raylib_cs;
+
+namespace HWEmu
+{
+    public class DrawableChip
+    {
+        public required Rectangle Rectangle { get; set; }
+    }
+
+    public class Chip : Connectable
+    {
+
+        public static void DrawChip()
+        {
+            //Raylib.DrawLineEx(and.IOs[0].Position, and.RightCircleCenter, lineThick, Color.DarkGray);
+        }
+
+        public static void CreateAChipInstance()
+        {
+
+        }
+
+        public static void FormDrawableChip(Chip chip)
+        {
+            foreach(IO io in chip.IOs)
+            {
+
+            }
+        }
+
+        public override void CheckIfInputShouldChange(Connector c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
