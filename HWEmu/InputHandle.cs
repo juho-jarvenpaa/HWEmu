d0fde5436f661779bde254197bf859bf67850adb
JJPaa <juhojarvenpaa98@gmail.com>
2026-03-29 13:11:42 +0300

    Refactor3

--- HWEmu/InputHandle.cs
+++ HWEmu/InputHandle.cs
@@ -11,176 +11,192 @@ namespace HWEmu
     {
         public static Vector2 MousePosVec2;
 
+        public static void TrySelectInput()
+        {
+            // Iterate over all ui items to see if something is close
+
+            foreach (var chip in Program.ProjectChips)
+            {
+                foreach (var io in chip.Inputs)
+                {
+                    if (Logic.PointCloseEnough(MousePosVec2, io.Position))
+                    {
+                        // found connection
+                        Connector c = new Connector() { OldOutput = Program.SelectedOldOutput, NewInput = io };
+                        c.State = Program.SelectedOldOutput.State;
+                        Program.Connectors.Add(c);
+                        Logic.ConnectorStateQueue.Add(c);
+                        break;
+                    }
+                }
+                if (Program.SelectedOldOutput == null)
+                {
+                    break;
+                }
+            }
+
+            foreach (var i in Program.inverters)
+            {
+                foreach (var io in i.Inputs)
+                {
+                    if (Logic.PointCloseEnough(MousePosVec2, io.Position))
+                    {
+                        // found connection
+                        Connector c = new Connector() { OldOutput = Program.SelectedOldOutput, NewInput = io };
+                        c.State = Program.SelectedOldOutput.State;
+                        Program.Connectors.Add(c);
+                        Logic.ConnectorStateQueue.Add(c);
+                        break;
+                    }
+                }
+                if (Program.SelectedOldOutput == null)
+                {
+                    break;
+                }
+            }
+            foreach (var or in Program.ors)
+            {
+                foreach (var io in or.Inputs)
+                {
+                    if (Logic.PointCloseEnough(MousePosVec2, io.Position))
+                    {
+                        // found connection
+                        Connector c = new Connector() { OldOutput = Program.SelectedOldOutput, NewInput = io };
+                        c.State = Program.SelectedOldOutput.State;
+                        Program.Connectors.Add(c);
+                        Logic.ConnectorStateQueue.Add(c);
+                        break;
+                    }
+                }
+                if (Program.SelectedOldOutput == null)
+                {
+                    break;
+                }
+            }
+            foreach (var and in Program.ands)
+            {
+                foreach (var io in and.Inputs)
+                {
+                    if (Logic.PointCloseEnough(MousePosVec2, io.Position))
+                    {
+                        Connector c = new Connector() { OldOutput = Program.SelectedOldOutput, NewInput = io };
+                        c.State = Program.SelectedOldOutput.State;
+                        Program.Connectors.Add(c);
+                        Logic.ConnectorStateQueue.Add(c);
+                        break;
+                    }
+                }
+                if (Program.SelectedOldOutput == null)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void TrySelectOutput()
+        {
+            // Iterate over all ui items to see if something is close
+
+            foreach (var chip in Program.ProjectChips)
+            {
+                foreach (var o in chip.Outputs)
+                {
+                    if (Logic.PointCloseEnough(MousePosVec2, o.Position))
+                    {
+                        Program.SelectedOldOutput = o;
+                        break;
+                    }
+                }
+                if (Program.SelectedOldOutput != null)
+                {
+                    break;
+                }
+            }
+
+            foreach (var or in Program.ors)
+            {
+                foreach (var o in or.Outputs)
+                {
+                    if (Logic.PointCloseEnough(MousePosVec2, o.Position))
+                    {
+                        Program.SelectedOldOutput = o;
+                        break;
+                    }
+                }
+                if (Program.SelectedOldOutput != null)
+                {
+                    break;
+                }
+            }
+
+            foreach (var and in Program.ands)
+            {
+                foreach (var o in and.Outputs)
+                {
+                    if (Logic.PointCloseEnough(MousePosVec2, o.Position))
+                    {
+                        Program.SelectedOldOutput = o;
+                        break;
+                    }
+                }
+                if (Program.SelectedOldOutput != null)
+                {
+                    break;
+                }
+            }
+
+            foreach (var i in Program.inverters)
+            {
+                foreach (var io in i.Outputs)
+                {
+                    if (Logic.PointCloseEnough(MousePosVec2, io.Position))
+                    {
+                        Program.SelectedOldOutput = io;
+                        break;
+                    }
+                }
+                if (Program.SelectedOldOutput != null)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void ProcessInputsWithShift()
+        {
+            Program.showLinkablePositions = true;
+
+            if (Raylib.IsMouseButtonDown(MouseButton.Left))
+            {
+                if (Program.SelectedOldOutput == null)
+                {
+                    TrySelectOutput();
+                }
+                else
+                {
+                    Program.draggingConnection = true;
+                }
+            }
+            else
+            {
+                if (Program.draggingConnection)
+                {
+                    TrySelectInput();
+                }
+
+                // Reset
+                Program.SelectedOldOutput = null;
+                Program.draggingConnection = false;
+            }
+        }
+
         public static void ProcessInputs()
         {
             MousePosVec2 = Raylib.GetMousePosition();
 
             if (Raylib.IsKeyDown(KeyboardKey.LeftShift) || Raylib.IsKeyDown(KeyboardKey.RightShift))
             {
-                Program.showLinkablePositions = true;
-
-                if (Raylib.IsMouseButtonDown(MouseButton.Left))
-                {
-                    if (Program.SelectedOldOutput == null)
-                    {
-                        // Iterate over all ui items to see if something is close
-
-                        foreach (var chip in Program.ProjectChips)
-                        {
-                            foreach (var o in chip.Outputs)
-                            {
-                                if (Logic.PointCloseEnough(MousePosVec2, o.Position))
-                                {
-                                    Program.SelectedOldOutput = o;
-                                    break;
-                                }
-                            }
-                            if (Program.SelectedOldOutput != null)
-                            {
-                                break;
-                            }
-                        }
-
-                        foreach (var or in Program.ors)
-                        {
-                            foreach (var o in or.Outputs)
-                            {
-                                if (Logic.PointCloseEnough(MousePosVec2, o.Position))
-                                {
-                                    Program.SelectedOldOutput = o;
-                                    break;
-                                }
-                            }
-                            if (Program.SelectedOldOutput != null)
-                            {
-                                break;
-                            }
-                        }
-
-                        foreach (var and in Program.ands)
-                        {
-                            foreach (var o in and.Outputs)
-                            {
-                                if (Logic.PointCloseEnough(MousePosVec2, o.Position))
-                                {
-                                    Program.SelectedOldOutput = o;
-                                    break;
-                                }
-                            }
-                            if (Program.SelectedOldOutput != null)
-                            {
-                                break;
-                            }
-                        }
-
-                        foreach (var i in Program.inverters)
-                        {
-                            foreach (var io in i.Outputs)
-                            {
-                                if (Logic.PointCloseEnough(MousePosVec2, io.Position))
-                                {
-                                    Program.SelectedOldOutput = io;
-                                    break;
-                                }
-                            }
-                            if (Program.SelectedOldOutput != null)
-                            {
-                                break;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Program.draggingConnection = true;
-                    }
-                }
-                else
-                {
-                    if (Program.draggingConnection)
-                    {
-                        // Iterate over all ui items to see if something is close
-
-                        foreach (var chip in Program.ProjectChips)
-                        {
-                            foreach (var io in chip.Inputs)
-                            {
-                                if (Logic.PointCloseEnough(MousePosVec2, io.Position))
-                                {
-                                    // found connection
-                                    Connector c = new Connector() { OldOutput = Program.SelectedOldOutput, NewInput = io };
-                                    c.State = Program.SelectedOldOutput.State;
-                                    Program.Connectors.Add(c);
-                                    Logic.ConnectorStateQueue.Add(c);
-                                    break;
-                                }
-                            }
-                            if (Program.SelectedOldOutput == null)
-                            {
-                                break;
-                            }
-                        }
-
-                        foreach (var i in Program.inverters)
-                        {
-                            foreach (var io in i.Inputs)
-                            {
-                                if (Logic.PointCloseEnough(MousePosVec2, io.Position))
-                                {
-                                    // found connection
-                                    Connector c = new Connector() { OldOutput = Program.SelectedOldOutput, NewInput = io };
-                                    c.State = Program.SelectedOldOutput.State;
-                                    Program.Connectors.Add(c);
-                                    Logic.ConnectorStateQueue.Add(c);
-                                    break;
-                                }
-                            }
-                            if (Program.SelectedOldOutput == null)
-                            {
-                                break;
-                            }
-                        }
-                        foreach (var or in Program.ors)
-                        {
-                            foreach (var io in or.Inputs)
-                            {
-                                if (Logic.PointCloseEnough(MousePosVec2, io.Position))
-                                {
-                                    // found connection
-                                    Connector c = new Connector() { OldOutput = Program.SelectedOldOutput, NewInput = io };
-                                    c.State = Program.SelectedOldOutput.State;
-                                    Program.Connectors.Add(c);
-                                    Logic.ConnectorStateQueue.Add(c);
-                                    break;
-                                }
-                            }
-                            if (Program.SelectedOldOutput == null)
-                            {
-                                break;
-                            }
-                        }
-                        foreach (var and in Program.ands)
-                        {
-                            foreach (var io in and.Inputs)
-                            {
-                                if (Logic.PointCloseEnough(MousePosVec2, io.Position))
-                                {
-                                    Connector c = new Connector() { OldOutput = Program.SelectedOldOutput, NewInput = io };
-                                    c.State = Program.SelectedOldOutput.State;
-                                    Program.Connectors.Add(c);
-                                    Logic.ConnectorStateQueue.Add(c);
-                                    break;
-                                }
-                            }
-                            if (Program.SelectedOldOutput == null)
-                            {
-                                break;
-                            }
-                        }
-                    }
-
-                    Program.SelectedOldOutput = null;
-                    Program.draggingConnection = false;
-                }
+                ProcessInputsWithShift();
             }
             else
             {
