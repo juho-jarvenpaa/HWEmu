2ebabffe3595eae699b605818cc350ebae515bff
JJPaa <juhojarvenpaa98@gmail.com>
2026-04-07 09:05:05 +0300

    temp

--- HWEmu/ChipCreator.cs
+++ HWEmu/ChipCreator.cs
@@ -10,22 +10,27 @@ namespace HWEmu
     {
         Selecting,
         SelectingAndDragging,
-        TypeName,
-        Saving
+        AskingChipName,
+        SavingInProgress
     }
 
     public static class ChipCreator
     {
         public static ChipCreatorMode Mode { get; set; } = ChipCreatorMode.Selecting;
-        public static List<Input> SelectedInputs { get; set; } = new List<Input>();
-        public static List<Output> SelectedOutputs { get; set; } = new List<Output>();
-        public static Vector2 SelectStartPoint { get; set; } = new Vector2();
+        public static Dictionary<string, Input> SelectedInputs { get; set; } = new Dictionary<string, Input>();
+        public static Dictionary<string, Output> SelectedOutputs { get; set; } = new Dictionary<string, Output>();
+        public static Vector2 MouseSelectStartPoint { get; set; } = new Vector2();
+        public static Vector2 MouseSelectEndpoint { get; set; } = new Vector2();
 
         public static void Save()
         {
             if(SelectedInputs.Count > 0 && SelectedOutputs.Count > 0)
             {
                 // Iterate over all inputs and outputs and form a truth table
+                foreach(var input in SelectedInputs.Values)
+                {
+                    
+                }
             }
         }
 
@@ -36,29 +41,44 @@ namespace HWEmu
                 case ChipCreatorMode.Selecting:
                     if (Raylib.IsMouseButtonDown(MouseButton.Left))
                     {
-                        Mode = ChipCreatorMode.Selecting;
-                        SelectStartPoint = InputHandle.MousePosVec2;
+                        Mode = ChipCreatorMode.SelectingAndDragging;
+                        MouseSelectStartPoint = InputHandle.MousePosVec2;
                     }
                     if (Raylib.IsKeyDown(KeyboardKey.Enter))
                     {
-                        Mode = ChipCreatorMode.TypeName;
+                        Mode = ChipCreatorMode.AskingChipName;
                     }
                     break;
                 case ChipCreatorMode.SelectingAndDragging:
-                    if(Raylib.IsMouseButtonReleased(MouseButton.Left))
+                    if(Raylib.IsMouseButtonDown(MouseButton.Left))
                     {
+
+                    }
+                    else
+                    {
+                        // This part adds the IO's to list
+                        // Triggers when left click is released
                         // Select IO's inside the painted area
+                        MouseSelectEndpoint = InputHandle.MousePosVec2;
+                        
+                        // TODO rethink how to deal with And, Or etc.
+                        //foreach (var item in )
+                        //{
+
+                        //}
+
                         Mode = ChipCreatorMode.Selecting;
                     }
                     break;
-                case ChipCreatorMode.TypeName:
+                case ChipCreatorMode.AskingChipName:
                     if(Raylib.IsKeyDown(KeyboardKey.Enter))
                     {
-                        Mode = ChipCreatorMode.Saving;
+                        Mode = ChipCreatorMode.SavingInProgress;
                         Save();
                     }
                     break;
-                case ChipCreatorMode.Saving:
+                case ChipCreatorMode.SavingInProgress:
+                    // After saving return to selecting
                     if(SelectedInputs.Count == 0)
                     {
                         Mode = ChipCreatorMode.Selecting;
